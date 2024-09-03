83b0ab23b74894f6e6697ab09d89db2b6fbc1eda
Pedro Schinke <pedro.schinke@outlook.com>
2024-09-03 13:56:49 -0300

    created available tags array

---
+++ js/availableTags.js
@@ -0,0 +1,14 @@
+export const availableTags = [
+    "React.js",
+    "Node.js",
+    "Data Science",
+    "HTML",
+    "CSS",
+    "Javascript",
+    "Typescript",
+    "PHP",
+    "Java",
+    "Python",
+    "C#",
+    "Laravel",
+]
